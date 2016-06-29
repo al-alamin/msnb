c1932817ce6e06fd84aa0c06c00cb113dd6eaa0e
tareqobaida <tareqbuet@gmail.com>
2016-06-29 15:30:15 -0500

    Pulling Masud's changes and merging

---
+++ MSNB/wsgi.py
@@ -0,0 +1,29 @@
+"""
+WSGI config for HSS_Obaida project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os, sys, site
+
+from django.core.wsgi import get_wsgi_application
+# linode specific settings
+# Add the site-packages of the chosen virtualenv to work with
+# site.addsitedir('/home/.virtualenvs/wcsenvpython3/lib/python3.4/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+# sys.path.append('/var/www/html/mystudynotebook.com/MSNB')
+# sys.path.append('/var/www/html/mystudynotebook.com/MSNB/MSNB')
+# Activate your virtual env
+# activate_env=os.path.expanduser("/home/.virtualenvs/wcsenvpython3/bin/activate_this.py")
+# execfile(activate_env, dict(__file__=activate_env))
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MSNB.settings")
+
+
+application = get_wsgi_application()
