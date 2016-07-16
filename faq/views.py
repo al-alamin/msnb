1287e7aca3efca9487f354a021e3a46f34195d56
tareqobaida <tareqbuet@gmail.com>
2016-07-16 02:55:50 -0500

    capability to search by category and tag added

--- faq/views.py
+++ faq/views.py
@@ -14,17 +14,24 @@ def faq(request):
     return render(request, 'faq/faq.html', context)
 
 
-def search_result(request):
+def search_result(request, cat=None, tag=None):
+    types = models.Type.objects.all().exclude(name='News')
+    faq_search_form = FaqSearchForm()
     if request.method == 'POST':
         faq_search_form = FaqSearchForm(request.POST)
         if faq_search_form.is_valid():
             search_result = faq_search_form.get_search_result()
-        types = models.Type.objects.all()
-        context = {
-            'faq_search_form':faq_search_form,
-            'search_result': search_result,
-            'types':types,
-        }
-        return render(request, 'faq/search_result.html', context)
+
+    elif cat is not None:
+        search_result = models.Question.objects.filter(category__name__iexact=cat)
+    elif tag is not None:
+        search_result = models.Question.objects.filter(tag__name__iexact=tag)
     else:
         redirect('faq')
+
+    context = {
+        'faq_search_form': faq_search_form,
+        'search_result': search_result,
+        'types': types,
+    }
+    return render(request, 'faq/search_result.html', context)
