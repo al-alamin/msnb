99c9d2c40428c59e5dbb30cb7f634a2075595ff9
tareqobaida <tareqbuet@gmail.com>
2016-08-08 15:04:14 -0500

    Added top breadcrumb in home page. Shuffled advisors list for home page

--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from models.models import Author
+from random import shuffle
 
 
 # Create your views here.
 def home(request):
     advisors_roles = ['Advisor','Reviewer','Presenter']
     advisors = Author.objects.filter(role__role__in=advisors_roles).distinct()
+    advisors = (list(advisors))  # to make it shuffleable
+    shuffle(advisors)  # in-place shuffling
     return render(request, "home/home.html", {'advisors':advisors})
 
 
