57db751ea5ba487974114c6d767872ab0f5421eb
Abu Obaida <tareqbuet@gmail.com>
2016-08-10 14:52:01 -0500

    update wsgi for linode

--- MSNB/wsgi.py
+++ MSNB/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HSS_Obaida project.
+WSGI config for MSNB.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,20 +10,22 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os, sys, site
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MSNB.settings")
 # linode specific settings
 # Add the site-packages of the chosen virtualenv to work with
-# site.addsitedir('/home/.virtualenvs/wcsenvpython3/lib/python3.4/site-packages')
+site.addsitedir('/home/.virtualenvs/wcsenvpython3/lib/python3.4/site-packages')
 
 # Add the app's directory to the PYTHONPATH
-# sys.path.append('/var/www/html/mystudynotebook.com/MSNB')
-# sys.path.append('/var/www/html/mystudynotebook.com/MSNB/MSNB')
+sys.path.append('/var/www/html/mystudynotebook.com/MSNB')
+sys.path.append('/var/www/html/mystudynotebook.com/MSNB/MSNB')
 # Activate your virtual env
-# activate_env=os.path.expanduser("/home/.virtualenvs/wcsenvpython3/bin/activate_this.py")
-# execfile(activate_env, dict(__file__=activate_env))
+activate_env=os.path.expanduser("/home/.virtualenvs/wcsenvpython3/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MSNB.settings")
+
 
 
 application = get_wsgi_application()
