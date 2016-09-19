588f7763185b2f430268506f7b0b836735266563
tareqobaida <tareqbuet@gmail.com>
2016-09-19 14:25:55 -0500

    Template files are copied to respective apps

--- faq/models.py
+++ faq/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from common.models import Category, Tag
 
 
-# Create your models here.
 class Question(models.Model):
     text = models.TextField(max_length=500)
     ans = models.TextField(max_length=500, blank=True)
-    category = models.ManyToManyField(Category, help_text= '**To make a question popular select "popular_faq" category.**')
+    category = models.ManyToManyField(Category,
+                                      help_text='**To make a question popular select "popular_faq" category.**')
     tag = models.ManyToManyField(Tag, blank=True)
     date = models.DateField(auto_now_add=True)
 
