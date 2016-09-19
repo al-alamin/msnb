9720db8eb9967bcc0e231f2989d10961dbd1d513
alalamin25 <mdabdullahalalaminp@gmail.com>
2016-09-19 19:51:32 +0600

    Pep8 sop/forms.py max length exceeded 80 charecters

--- sop/forms.py
+++ sop/forms.py
@@ -7,9 +7,12 @@ SUPPORTED_SOP_FILE_TYPES = ['application/msword',
 
 class SOPSubmitForm(forms.Form):
     attrs = {"class": "form-control", 'required': 'required'}
-    name = forms.CharField(max_length=50, label='Name', widget=forms.TextInput(attrs=attrs))
-    email = forms.EmailField(max_length=30, label='Email', widget=forms.TextInput(attrs=attrs))
-    msg = forms.CharField(max_length=500, label='Message', widget=forms.Textarea(attrs=attrs))
+    name = forms.CharField(
+        max_length=50, label='Name', widget=forms.TextInput(attrs=attrs))
+    email = forms.EmailField(
+        max_length=30, label='Email', widget=forms.TextInput(attrs=attrs))
+    msg = forms.CharField(
+        max_length=500, label='Message', widget=forms.Textarea(attrs=attrs))
     file_attrs = {'required': 'required',
                   'onchange': "this.parentNode.nextSibling.value = this.value",
                   }
@@ -21,12 +24,14 @@ class SOPSubmitForm(forms.Form):
         if sop_file.size > 5 * 1024 * 1024:
             raise forms.ValidationError("File size exceeded 5MB limit")
         if sop_file.content_type not in SUPPORTED_SOP_FILE_TYPES:
-            raise forms.ValidationError("Unsupported file type. Please upload either .doc or .docx file")
+            raise forms.ValidationError(
+                "Unsupported file type. Please upload either .doc or .docx file")
 
     def email_SOP(self, file):
         name = self.cleaned_data['name']
         from_email = self.cleaned_data['email']
         msg = self.cleaned_data['msg']
         subject = "{0} submitted a SOP for review".format(name)
-        email_success = send_mail(subject, msg, from_email=from_email, attachmets=file)
+        email_success = send_mail(
+            subject, msg, from_email=from_email, attachmets=file)
         return email_success
